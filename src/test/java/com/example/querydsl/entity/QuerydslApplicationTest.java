880faff578960dee7fb70bcc1a217d6caa7cf54e
minu <made_power@naver.com>
2023-04-13 22:52:29 +0900

    초기설정
    초기설정 및 의존성 추가

--- src/test/java/com/example/querydsl/entity/QuerydslApplicationTest.java
+++ src/test/java/com/example/querydsl/entity/QuerydslApplicationTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTest {
     @Autowired
     EntityManager em;
