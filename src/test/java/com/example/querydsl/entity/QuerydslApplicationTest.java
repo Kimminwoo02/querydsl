89e50fcb3fca44fbf58b101f7f2f9e634a0084a3
minu <made_power@naver.com>
2023-04-13 17:53:22 +0900

    Querydsl 설정
    QueryDsl 학습을 위한 초기 셋팅

---
+++ src/test/java/com/example/querydsl/entity/QuerydslApplicationTest.java
@@ -0,0 +1,34 @@
+package com.example.querydsl.entity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTest {
+    @Autowired
+    EntityManager em;
+    @Test
+    void contextLoad(){
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+
+}
