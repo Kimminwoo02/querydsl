a174204a33a010f2273e2d98f9995a5c392bf28e
minu <made_power@naver.com>
2023-04-14 21:50:36 +0900

    검색 조건 쿼리
    검색 조건쿼리의 방법들을 학습

--- src/test/java/com/example/querydsl/entity/QuerydslBasicTest.java
+++ src/test/java/com/example/querydsl/entity/QuerydslBasicTest.java
@@ -59,4 +59,26 @@ public class QuerydslBasicTest {
                 .fetchOne();
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
+    @Test
+    public void search(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.between(10,30)))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchParam(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                    )
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 }
