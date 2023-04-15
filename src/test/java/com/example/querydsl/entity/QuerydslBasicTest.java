d85d8d67c20ed4d3a0586a77c3d0cbfb588a4ef7
minu <made_power@naver.com>
2023-04-16 02:29:14 +0900

    정렬
    정렬에 대한 사항을 학습

--- src/test/java/com/example/querydsl/entity/QuerydslBasicTest.java
+++ src/test/java/com/example/querydsl/entity/QuerydslBasicTest.java
@@ -110,4 +110,28 @@ public class QuerydslBasicTest {
 
     }
 
+    /**
+     *  1. 나이 내림차순
+     *  2. 이름 올림차순
+     *  2에서 회원이름 없으면 마지막에 출력
+     */
+    @Test
+    public void sort(){
+        em.persist(new Member(null,100));
+        em.persist(new Member("member5",100));
+        em.persist(new Member("member6",100));
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member5.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
+    }
+
 }
