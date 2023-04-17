88570dbe9f18dfaf7888f83d4af11bedcc180891
minu <made_power@naver.com>
2023-04-17 20:52:03 +0900

    상수, concat
    상수 및 문자 더하는 방법에 대하여 학습

--- src/test/java/com/example/querydsl/entity/QuerydslBasicTest.java
+++ src/test/java/com/example/querydsl/entity/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package com.example.querydsl.entity;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import net.bytebuddy.build.ToStringPlugin;
@@ -407,6 +408,32 @@ public class QuerydslBasicTest {
             }
 
         }
+    @Test
+    public void constant(){
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void concat(){
+        //{username}_{age}
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+
+        }
+    }
+
 
 
     }
