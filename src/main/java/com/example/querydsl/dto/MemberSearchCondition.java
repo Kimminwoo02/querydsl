b4171cdeda3e6526d83392b913bcc163588667d1
minu <made_power@naver.com>
2023-04-20 14:49:55 +0900

    Builder를 이용한 동적쿼리 조회
    조건을 추가하기 위한 동적쿼리를 사용하기 위해 Builder에 대한 학습을  했다.

---
+++ src/main/java/com/example/querydsl/dto/MemberSearchCondition.java
@@ -0,0 +1,13 @@
+package com.example.querydsl.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberSearchCondition {
+    //회원명, 팀명, 나이(ageGoe,ageLoe
+    private String username;
+    private String teamName;
+    private Integer ageGoe;
+    private Integer ageLoe;
+
+}
--- src/test/java/com/example/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/example/querydsl/repository/MemberJpaRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.example.querydsl.repository;
 
+import com.example.querydsl.dto.MemberSearchCondition;
+import com.example.querydsl.dto.MemberTeamDto;
 import com.example.querydsl.entity.Member;
 
+import com.example.querydsl.entity.Team;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,5 +41,32 @@ class MemberJpaRepositoryTest {
         assertThat(result2).containsExactly(member);
     }
 
+    @Test
+    public void searchTest(){
+        Team teamA = new Team("TeamA");
+        Team teamB = new Team("TeamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("Member1", 10, teamA);
+        Member member2 = new Member("Member2", 20, teamA);
+        Member member3 = new Member("Member3", 30, teamB);
+        Member member4 = new Member("Member4", 38, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.serachByBuilder(condition);
+        assertThat(result).extracting("username").containsExactly("member4");
+
+    }
 
 }
